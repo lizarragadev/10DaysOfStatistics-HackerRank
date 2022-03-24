aacc2c06aab955eb353582db0f19fa8c91499aab
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-03-24 19:05:34 -0400

    Add templates code

---
+++ src/main/kotlin/TemplateCode.kt
@@ -0,0 +1,18 @@
+import java.io.PrintWriter
+
+/**
+ * Tutorials > 10 Days of Statistics > Day 4: Name
+ *
+ * https://www.hackerrank.com/challenges/s10-quartiles/
+ * @author lizarragadev
+ *
+ */
+
+fun main() {
+    _writer.name()
+    _writer.flush()
+}
+
+fun PrintWriter.name() {
+
+}
